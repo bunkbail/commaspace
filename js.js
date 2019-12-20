87aa8edf3f8b0f185d0f0fdf22e668af1bcb40f5
Nabil Danial <nabildanial.93@gmail.com>
2019-12-21 02:53:45 +0800

    Update js.js

--- js.js
+++ js.js
@@ -3,3 +3,12 @@ function replace() {
   var txtBoxModified = txtBox.replace(/, /g,'\n');
   document.getElementById("txtbox").value = txtBoxModified;
 }
+
+var button = document.getElementById("copyToClipboard"),
+    input = document.getElementById("txtbox");
+
+    button.addEventListener("click", function(event) {
+      event.preventDefault();
+      input.select();
+      document.execCommand("copy");
+    });
